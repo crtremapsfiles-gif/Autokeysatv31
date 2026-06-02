c8d0d3572ee90ba6dbf740d199f75a60face8ec8
crtremapsfiles-gif <crtremapsfiles@gmail.com>
2026-06-02 15:59:01 +0200

    Update config.js

--- AutokeysPro_SAT_V3_1/src/config.js
+++ AutokeysPro_SAT_V3_1/src/config.js
@@ -2,4 +2,4 @@
 // Pega aquí tu ANON PUBLIC KEY de Supabase. NO uses service_role.
 
 export const SUPABASE_URL = "https://zvsekmvzkmywwostezwf.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_JaC5CiZOmuwpm0uykhUSXA_t1lC-zEV";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2c2VrbXZ6a215d3dvc3RlendmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyNjA2MDAsImV4cCI6MjA5NTgzNjYwMH0.6PGVHDSPz0o7g8XP2OXSM8RpAupKGxbhaSLw3quUORQ";
